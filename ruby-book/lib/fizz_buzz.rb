6d2d8591d3c29d022487c2ec8395fd33c28c6df7
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-03 17:21:09 +0900

    test 完了

--- ruby-book/lib/fizz_buzz.rb
+++ ruby-book/lib/fizz_buzz.rb
@@ -1,17 +1,29 @@
 def fizz_buzz(n)
   if n % 15 == 0
-    puts 'FizzBuzz'
+    'FizzBuzz'
   elsif n % 3 == 0
-    puts 'Fizz'
+    'Fizz'
   elsif n % 5 == 0
-    puts 'Buzz'
+    'Buzz'
   else
     n.to_s
   end
 end
 
-n = 1
-while n <= 100 do
-  puts fizz_buzz(n)
-  n += 1
+require 'minitest/autorun'
+
+class FizzBuzzTest < Minitest::Test
+  def test_fizz_buzz
+    assert_equal '1', fizz_buzz(1)
+    assert_equal '2', fizz_buzz(2)
+    assert_equal 'Fizz', fizz_buzz(3)
+    assert_equal 'Buzz', fizz_buzz(5)
+    assert_equal 'FizzBuzz', fizz_buzz(15)
+  end
 end
+
+# n = 1
+# while n <= 100 do
+#   puts fizz_buzz(n)
+#   n += 1
+# end
