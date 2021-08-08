66f4ed8f8d22d63523302d988b7dffb77b5112fb
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-08 09:06:18 +0900

    case文を使うまで

--- ruby-book/block.rb
+++ ruby-book/block.rb
@@ -1,16 +1,16 @@
-# numbers = [1,2,3,4]
-# sum = 0
-# numbers.each do |n|
-#   sum_value = n.even? ? n*10:n
-#   sum += sum_value
-# end
-# puts sum
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n|
+  sum_value = n.even? ? n*10:n
+  sum += sum_value
+end
+puts sum
 
 # --- 配列や文字列の一部を抜き出す ---
-# a = [1,2,3,4,5]
-# puts a[1..3]
-# a = 'abcdef'
-# puts a[1..3]
+a = [1,2,3,4,5]
+puts a[1..3]
+a = 'abcdef'
+puts a[1..3]
 
 # --- n以上m以下、n以上m未満の判定をする ---
 def liquid?(temperature)
@@ -18,3 +18,21 @@ def liquid?(temperature)
 end
 puts liquid?(50)
 
+# --- case文で使う ---
+def charge(age)
+  case age
+  when 0..5
+    0
+  when 6..12
+    300
+  when 13..18
+    600
+  else
+    1000
+  end
+end
+
+puts charge(3)
+puts charge(12)
+puts charge(16)
+puts charge(25)
