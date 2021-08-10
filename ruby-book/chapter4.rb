e095f98474df2e00714a718b4f5cb4123714515c
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-10 11:22:33 +0900

    4.5.5終了

--- ruby-book/chapter4.rb
+++ ruby-book/chapter4.rb
@@ -43,3 +43,18 @@ puts (1...5).to_a
 puts "splat展開"
 puts [*1..5]
 puts [*1...5]
+
+# 4.5.5 繰り返し処理を行う ---
+numbers = (1..4).to_a
+sum = 0
+numbers.each {|n| sum += n}
+puts sum
+## 範囲オブジェクトに対して直接eachメソッドを呼び出す
+sum = 0
+(1..4).each {|n| sum += n}
+puts sum
+## stepメソッドで値を増やす間隔を指定できる
+numbers = []
+### 1から10まで2つ飛ばしで繰り返し処理を行う
+(1..10).step(2) {|n| numbers << n}
+puts numbers
