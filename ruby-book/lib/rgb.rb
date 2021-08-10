f0ca5a04c9c484a6e9ef8077f2660d326b56da90
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-10 11:58:27 +0900

    rgbファイル、リファクタリング完了

--- ruby-book/lib/rgb.rb
+++ ruby-book/lib/rgb.rb
@@ -1,3 +1,22 @@
+# def to_hex(r, g, b)
+#   '#' +
+#   r.to_s(16).rjust(2, '0') +
+#   g.to_s(16).rjust(2, '0') +
+#   b.to_s(16).rjust(2, '0')
+# end
+
+## --- 上記をリファクタリング！ ---
+# def to_hex(r, g, b)
+#   hex = '#'
+#   [r, g, b].each do |n|
+#     hex += n.to_s(16).rjust(2, '0')
+#   end
+#   hex
+# end
+
+## --- さらにリファクタリング! ---
 def to_hex(r, g, b)
-  '#000000'
+  [r, g, b].inject('#') do |hex, n|
+    hex + n.to_s(16).rjust(2, '0')
+  end
 end
