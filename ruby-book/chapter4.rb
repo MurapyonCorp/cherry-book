db5e6b162613604e0826fe9f85a0e303ed34dac1
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-14 21:05:11 +0900

    4.7.7 終了

--- ruby-book/chapter4.rb
+++ ruby-book/chapter4.rb
@@ -142,3 +142,11 @@ puts a.push(b)
 puts
 puts a.push(*b)
 puts
+puts "4.7.7 メソッドの可変長引数 ---"
+def greeting(*names)
+  "#{names.join('と')}, こんにちは！ "
+end
+puts greeting('田中さん')
+puts greeting('田中さん','鈴木さん')
+puts greeting('田中さん','鈴木さん','佐藤さん')
+puts
