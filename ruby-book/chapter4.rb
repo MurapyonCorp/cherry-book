e2a4ca2f24cbcb4525853dc26bad4428775d7c1f
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-17 15:00:27 +0900

    4.8.4 終了

--- ruby-book/chapter4.rb
+++ ruby-book/chapter4.rb
@@ -227,3 +227,30 @@ fruits.each.with_index(1) {|fruit, i| puts "#{i}: #{fruit}" }
 puts
 puts print fruits.map.with_index(10) {|fruit, i| "#{i}: #{fruit}" }
 puts
+puts "4.8.4 配列がブロック引数に渡される場合 ---"
+dimensions = [
+  [10,20],
+  [30,40],
+  [50,60]
+]
+areas = []
+# dimensions.each do |dimension|
+#   length = dimension[0]
+#   width = dimension[1]
+#   areas << length * width
+# end
+dimensions.each do |length, width|
+  areas << length * width
+end
+puts print areas
+puts
+
+dimensions.each do |length, width, foo, bar|
+  p [length, width, foo, bar]
+end
+puts
+
+dimensions.each_with_index do |(length, width), i|
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+puts
