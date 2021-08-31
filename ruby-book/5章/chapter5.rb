1df886ad5d342053e4b5659d3d2c1ded714acbe2
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-08-31 21:34:50 +0900

    5.1.2 終了

--- ruby-book/5章/chapter5.rb
+++ ruby-book/5章/chapter5.rb
@@ -1,3 +1,9 @@
 # 5.1 イントロダクション
 puts "5.1.1 この章のこの章の例題 ：長さの単位変換プログラム ---"
 puts "・メートル、フィート、インチの単位を相互変換する"
+puts
+puts "5.1.2 長さの単位プログラムの実行例 ---"
+puts convert_length(1,'m', 'in')
+puts conveert_length(15, 'in', 'm')
+puts convert_length(35000, 'ft', 'm')
+puts
