b98ae010bd96231bc715ff4beb7fe4fbcf36700d
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-09-17 21:11:06 +0900

    6.2.1 finish

--- ruby-book/6章/chapter6.rb
+++ ruby-book/6章/chapter6.rb
@@ -28,3 +28,20 @@ convert_hash_syntax(old_syntax)
 # -- 6.1.3 この章で学ぶこと
 # ・正規表現そのもの
 # ・Rubyにおける正規表現オブジェクト
+
+# ! 6.2 正規表現って何？
+# -- 6.2.1 正規表現の便利さを知る
+# プログラミング用語っぽい文字列を抜き出すプログラムを書いてみよう
+text = <<TEXT
+I love Ruby.
+Python is a great language.
+Java and JavaScript are different.
+TEXT
+text.scan(/[A-Z][A-Za-z]+/)   # scanメソッドの引数部分が正規表現になる
+
+# 次は文字列変換をしてみよう
+text = <<TEXT
+私の郵便番号は1234567です。
+僕の住所は6770056 兵庫県西脇市板波町1234だよ。
+TEXT
+puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
