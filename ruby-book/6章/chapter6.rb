22c44e0221d2b4a5d2b49f99fa5d6a839e13f3fa
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-09-19 11:24:36 +0900

    6.3.3 finish

--- ruby-book/6章/chapter6.rb
+++ ruby-book/6章/chapter6.rb
@@ -75,3 +75,17 @@ m[3]
 m[2, 2]
 m[-1]
 m[1..3]
+
+# -- 6.3.3 キャプチャの結果に名前を付ける
+text = '私の誕生日は1977年7月17日です。'
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+m[:year]
+m[:month]
+m[:day]
+m['year']
+m[2]
+
+text = '私の誕生日は1977年7月17日です。'
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  puts "#{year}/#{month}/#{day}"
+end
