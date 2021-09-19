5bc284c5867a1db98b6d5a1dba3dd795dafd3d17
Ryo Murashige <s21ah094@seinan-gakuin.jp>
2021-09-19 12:00:34 +0900

    6.3.4 finish

--- ruby-book/6章/chapter6.rb
+++ ruby-book/6章/chapter6.rb
@@ -89,3 +89,52 @@ text = '私の誕生日は1977年7月17日です。'
 if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
   puts "#{year}/#{month}/#{day}"
 end
+
+-- 6.3.4 正規表現と組み合わせると便利なStringクラスのメソッド
+◇scan
+'123 456 789'.scan(/\d+/)
+'1977年7月17日 2016年12月31日'.scan(/(\d+)年(\d+)月(\d+)日/)
+'1977年7月17日 2016年12月31日'.scan(/(?:\d+)年(?:\d+)月(?:\d+)日/)
+'1977年7月17日 2016年12月31日'.scan(/\d+年\d+月\d+日/)
+
+◇[], slice, slice!
+text = '郵便番号は123-4567です'
+text[/\d{3}-\d{4}/]
+
+text = '123-4567 456-7890'
+text[/\d{3}-\d{4}/]
+
+text = '私の誕生日は1977年7月17日です。'
+text[/(\d+)年(\d+)月(\d+)日/, 3]
+text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, :day]
+text[/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, 'day']
+
+text = '郵便番号は123-4567です'
+text.slice(/\d{3}-\d{4}/)
+text = '私の誕生日は1977年7月17日です。'
+text.slice(/(\d+)年(\d+)月(\d+)日/, 3)
+text = '郵便番号は123-4567です'
+text.slice!(/\d{3}-\d{4}/)
+text
+
+◇split
+text = '123,456-789'
+text.split(',')
+text.split(/,|-/)
+
+◇gsub, gsub!
+text = '123,456-789'
+text.gsub(',', ':')
+text.gsub(/,|-/, ':')
+
+text = '私の誕生日は1977年7月17日です。'
+text.gsub(/(\d+)年(\d+)月(\d+)日/, '\1-\2-\3')
+text.gsub(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/, '\k<year>-\k<month>-\k<day>')
+
+text = '123,456-789'
+hash = {',' => ':', '-' => '/'}
+text.gsub(/,|-/, hash)
+text.gsub(/,|-/){|matched| matched == ',' ? ':' : '/'}
+
+text.gsub!(/,|-/, ':')
+text
