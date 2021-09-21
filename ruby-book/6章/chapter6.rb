709897fc5713b3e2c3084a8d6f0de602c31dbb14
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-21 17:51:02 +0900

    6.5.2 finish

--- ruby-book/6章/chapter6.rb
+++ ruby-book/6章/chapter6.rb
@@ -159,3 +159,14 @@ Regexp.new('\d{3}-\d{4}')
 %r{http://example\.com}
 pattern = '\d{3}-\d{4}'
 '123-4567' =~ /#{pattern}/
+
+-- 6.5.2 case文で正規表を使う
+  text = '03-1234-5678'
+  case text
+  when /^\d{3}-\d{4}$/
+    puts '郵便番号です'
+  when /^\d{4}\/\d{1,2}\/\d{1,2}$/
+    puts '日付です'
+  when /^\d+-\d+-\d+$/
+    puts '電話番号です'
+  end
