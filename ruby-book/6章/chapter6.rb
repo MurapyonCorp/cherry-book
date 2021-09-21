6ed733a613164bbddb6a5b786c3f15159e09ed3d
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-21 18:11:56 +0900

    6.5.4 finish

--- ruby-book/6章/chapter6.rb
+++ ruby-book/6章/chapter6.rb
@@ -192,3 +192,13 @@ regexp = Regexp.new(pattern, Regexp::EXTENDED)
 '123-4567' =~ regexp
 regexp = Regexp.new('Hello.Bye', Regexp::IGNORECASE | Regexp::MULTILINE)
 "HELLO\nBYE" =~ regexp
+
+-- 6.5.4 組み込み変数でマッチの結果を取得する
+text = '私の誕生日は1977年7月17日です。'
+text =~ /(\d+)年(\d+)月(\d+)日/
+$~
+$&
+$1
+$2
+$3
+$+
