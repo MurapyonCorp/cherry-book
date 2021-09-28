ac27402e4e3cddde9437f06cb97442a83f5be4d3
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-09-28 10:40:30 +0900

    7.5.3 finish

--- ruby-book/7章/chapter7.rb
+++ ruby-book/7章/chapter7.rb
@@ -233,3 +233,26 @@ end
 Foo.bar
 foo = Foo.new
 foo.baz
+
+-- 7.5.3 クラスメソッドをインスタンスメソッドで呼び出す
+class CallInstance
+  attr_reader :name, :price
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  # 金額を整形するクラスメソッド
+  def self.format_price(price)
+    "#{price}円"
+  end
+
+  def to_s
+    # インスタンスメソッドからクラスメソッドを呼び出す
+    formatted_price = CallInstance.format_price(price)
+    "name: #{name}, price: #{formatted_price}"
+  end
+end
+
+product = CallInstance.new('A great movie', 1000)
+product.to_s
