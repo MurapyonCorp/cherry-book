47b9e83fac5f359e34a504886bbe3dee79fb72c1
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-07 22:00:05 +0900

    7.10.3 finish

--- ruby-book/7章/chapter7.rb
+++ ruby-book/7章/chapter7.rb
@@ -648,3 +648,15 @@ cd.to_s
 # end
 # user = User.new
 # user.freeze
+
+-- 7.10.3 ネストしたクラスの定義
+# class User
+#   class BloodType
+#     attr_reader :type
+#     def initialize(type)
+#       @type = type
+#     end
+#   end
+# end
+# blood_type = User::BloodType.new('B')
+# blood_type.type
