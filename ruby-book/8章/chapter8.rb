816ed2d73049e0f968e4ef7d5c5f939b8a23f20b
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-09 16:56:33 +0900

    8.1.1 finish

---
+++ ruby-book/8章/chapter8.rb
@@ -0,0 +1,20 @@
+< 第8章 モジュールを理解する >
+! 8.1 イントロダクション
+-- 8.1.1 この章の例題：deep_freezeメソッド
+# class Team
+#   # COUNTRIES = ['Japan', 'US', 'India'].freeze
+#   COUNTRIES = ['Japan'.freeze, 'US'.freeze, 'India'.freeze].freeze
+# end
+上記のように全てにfreezeをつけるのは面倒だ
+
+# class Team
+#   COUNTRIES = deep_freeze(['Japan', 'US', 'India'])
+# end
+# Team::COUNTRIES.frozen? => true
+# Team::COUNTRIES.all?{|country| country.frozen?} => true
+
+# class Bank
+#   CURRENCIES = deep_freeze({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
+# end
+# Bank::CURRENCIES.frozen? => true
+# Bank::CURRENCIES.all?({|key, value| key.frozen? && value.frozen?}) => true
