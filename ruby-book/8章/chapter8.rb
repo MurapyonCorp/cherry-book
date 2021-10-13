bb3eaee90963d1ab635d06da499883575d6e0de9
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-13 10:51:19 +0900

    8.3.2 finish

--- ruby-book/8章/chapter8.rb
+++ ruby-book/8章/chapter8.rb
@@ -72,3 +72,19 @@
 # product.title
 # user = User.new
 # user.name
+
+-- 8.3.2 モジュールをextendする
+# module Loggable
+#   def log(text)
+#     puts "[LOG] #{text}"
+#   end
+# end
+
+# class Product
+#   extend Loggable
+#   def self.create_products(names)
+#     log 'create_products is called.'
+#   end
+# end
+# Product.create_products([])
+# Product.log('Hello.')
