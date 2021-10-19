c7439a34eec902b3c41aea13cb5d5fd23354bb25
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-19 09:55:09 +0900

    8.9.1 finish

--- ruby-book/8章/chapter8.rb
+++ ruby-book/8章/chapter8.rb
@@ -383,3 +383,34 @@ String    Numeric    Array    Hash
 # AwesomeAPI.debug_mode = true
 # AwesomeAPI.base_url
 # AwesomeAPI.debug_mode
+
+! 8.9 モジュールに関する高度な話題
+-- 8.9.1 メソッド探索のルールを理解する
+# module A
+#   def to_s
+#     "<A>#{super}"
+#   end
+# end
+
+# module B
+#   def to_s
+#     "<B>#{super}"
+#   end
+# end
+
+# class Product
+#   def to_s
+#     "<Product>#{super}"
+#   end
+# end
+
+# class DVD < Product
+#   include A
+#   include B
+#   def to_s
+#     "<DVD>#{super}"
+#   end
+# end
+# dvd = DVD.new
+# dvd.to_s
+# DVD.ancestors
