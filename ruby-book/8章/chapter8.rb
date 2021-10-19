0807d5f594a9db40be2c0e6bfd10a0d64938ffc2
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-19 10:23:02 +0900

    8.9.5 finish

--- ruby-book/8章/chapter8.rb
+++ ruby-book/8章/chapter8.rb
@@ -472,3 +472,25 @@ String    Numeric    Array    Hash
 
 # product = Product.new
 # product.name
+
+-- 8.9.5 有効範囲を限定できるrefinements
+# module StringShuffle
+#   refine String do
+#     def shuffle
+#       chars.shuffle.join
+#     end
+#   end
+# end
+
+# class User
+#   using StringShuffle
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def shuffled_name
+#     @name.shuffle
+#   end
+# end
+# user = User.new('Alice')
+# user.shuffled_name
