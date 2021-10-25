227c6bbbf764c749d8ddb6e072262e9356a17705
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-25 10:53:24 +0900

    9.2.8 finish

--- ruby-book/9章/chapter9.rb
+++ ruby-book/9章/chapter9.rb
@@ -166,3 +166,27 @@ NoMethodError
 # rescue
 #   puts 'その他のエラーです'
 # end
+
+-- 9.2.8 例外発生時にもう一度処理をやり直すretry
+・retryを用いた構文
+# begin
+#   # 例外が発生するかもしれない処理
+# rescue
+#   retry   # 処理をやり直す
+# end
+カウンタ変数を用いてretryの回数を制限することが望ましい。
+
+# retry_count = 0
+# begin
+#   puts "処理を開始します。"
+#   # わざと例外を発生させる
+#   1 / 0
+# rescue
+#   retry_count += 1
+#   if retry_count <= 3
+#     puts "retryします。(#{retry_count}回目)"
+#     retry
+#   else
+#     puts "retryに失敗しました。"
+#   end
+# end
