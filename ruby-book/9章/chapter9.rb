0ab520c59500dc94ceb1bd0b611e6ab8bd6e9435
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-10-25 09:38:05 +0900

    9.2.3 finish

--- ruby-book/9章/chapter9.rb
+++ ruby-book/9章/chapter9.rb
@@ -49,3 +49,30 @@
 # end
 # # 例外処理を組み込んだためrubyコマンドでも最後まで実行可能
 # puts 'End.'
+
+-- 9.2.3 例外処理の流れ
+# method_1にだけ例外処理を記述する
+# def method_1
+#   puts 'method_1 start.'
+#   begin
+#     method_2
+#   rescue => exception
+#     puts '例外が発生しました'
+#   end
+#   puts 'method_1 end.'
+# end
+
+# def method_2
+#   puts 'method_2 start.'
+#   method_3
+#   puts 'method_2 end.'
+# end
+
+# def method_3
+#   puts 'method_3 start.'
+#   # ZeroDivisionErrorを発生させる
+#   1 / 0
+#   puts 'method_3 end.'
+# end
+# # 処理を開始する
+# method_1
