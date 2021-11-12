d620ce2559e013c1cef3d72bd5df245164e2e33c
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2021-11-12 13:20:05 +0900

    10.3.3 finish

--- ruby-book/10章/chapter10.rb
+++ ruby-book/10章/chapter10.rb
@@ -89,3 +89,17 @@ add_proc = proc{|a, b| a + b}
 # # Procオブジェクトを作成し、それをブロックの代わりとしてgreetingメソッドに渡す
 # repeat_proc = Proc.new{|text| text * 2}
 # greeting(&repeat_proc)
+
+--　10.3.3 Procオブジェクトを普通の引数として渡す
+次のコードはgreetingメソッドでブロックを受け取るのではなく、Procオブジェクトを普通の引数として受け取って実行するコード例です。
+# ブロックではなく、1個のProcオブジェクトを引数として受け取る(&を付けない)
+# def greeting(arrange_proc)
+#   puts 'おはよう'
+#   text = arrange_proc.call('こんにちは')
+#   puts text
+#   puts 'こんばんは'
+# end
+
+# # Procオブジェクトを普通の引数としてgreetingメソッドに渡す(＆を付けない)
+# repeat_proc = Proc.new{|text| text * 2}
+# greeting(repeat_proc)
