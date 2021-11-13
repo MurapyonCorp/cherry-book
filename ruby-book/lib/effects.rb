d6ef534829de36c2c210f469d998045aaa49f3c6
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2021-11-13 11:32:22 +0900

    echo

--- ruby-book/lib/effects.rb
+++ ruby-book/lib/effects.rb
@@ -1,7 +1,15 @@
 module Effects
   def self.reverse
     ->(words) do
-      words.split('').map(&:reverse).join('')
+      words.split(' ').map(&:reverse).join(' ')
+    end
+  end
+
+  def self.echo(rate)
+    ->(words) do
+      # スペースならそのまま返す
+      # スペース以外ならその文字を指定された回数だけ繰り返す
+      words.chars.map{|c| c == ' ' ? c : c * rate}.join
     end
   end
 end
